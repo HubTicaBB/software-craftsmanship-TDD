b9e8c04f067dc170caa9dfc80b6dabc73b61b1b1
HubTicaBB <tijana.ilic@volvocars.com>
2023-04-06 14:25:46 +0200

    Task 4 - Add_WithCustomSeparator_ReturnsSum - pass

--- Kata2/Kata2/StringCalculator.cs
+++ Kata2/Kata2/StringCalculator.cs
@@ -2,7 +2,7 @@
 
 public class StringCalculator
 {
-    public static readonly string[] delimiters = { ",", "\n" };
+    public static readonly string[] defaultDelimiters = { ",", "\n" };
 
     public int Add(string numbers)
     {
@@ -25,6 +25,22 @@ public class StringCalculator
 
     private string[] Split(string numbers)
     {
+        var customDelimiter = "";
+        var delimiterStartTag = "//";
+        var delimiterEndTag = "\n";
+
+        if (numbers.StartsWith(delimiterStartTag))
+        {
+            var from = numbers.IndexOf(delimiterStartTag) + delimiterStartTag.Length;
+            var to = numbers.IndexOf(delimiterEndTag);
+            customDelimiter = numbers[from..to];
+            numbers = numbers[to..];
+        }
+
+        string[] delimiters = customDelimiter != string.Empty
+            ? new string[] { customDelimiter }
+            : defaultDelimiters;
+
         return numbers.Split(delimiters, StringSplitOptions.None);
     }
 }
